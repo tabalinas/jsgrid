55b2c5714f49faa8d34b3cbf50252f347a2801ad
Artem Tabalin <tabalinas@gmail.com>
2014-12-14 17:54:21 +0300

    Site: Demos page styling

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(function() {
+    var DEMO_SELECTED_CLASS = "demo-selected";
+
+    var $links = $(".demos-navigation").find("a");
+
+    $links.click(function(e) {
+        $links.removeClass(DEMO_SELECTED_CLASS);
+        $(e.target).closest("a").addClass(DEMO_SELECTED_CLASS);
+    });
+});
