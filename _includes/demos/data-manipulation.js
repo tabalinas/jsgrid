6b212a2ed8095432521ee9ea6b5b1361a0a3a425
Artem Tabalin <tabalinas@gmail.com>
2015-02-26 01:31:15 +0300

    Site: Data manipulation demo with external dialog

--- _includes/demos/data-manipulation.js
+++ _includes/demos/data-manipulation.js
@@ -1,45 +1,70 @@
 $(function() {
 
     $("#jsGrid").jsGrid({
-        height: "40%",
+        height: "70%",
         width: "100%",
-
         editing: true,
         autoload: true,
-
+        paging: true,
         deleteConfirm: function(item) {
             return "The client \"" + item.Name + "\" will be removed. Are you sure?";
         },
-
+        rowClick: function(args) {
+            showDetailsDialog("Edit", args.item);
+        },
         controller: db,
-
         fields: [
             { name: "Name", type: "text", width: 150 },
             { name: "Age", type: "number", width: 50 },
             { name: "Address", type: "text", width: 200 },
             { name: "Country", type: "select", items: db.countries, valueField: "Id", textField: "Name" },
             { name: "Married", type: "checkbox", title: "Is Married", sorting: false },
-            { type: "control", modeSwitchButton: false, editButton: false }
+            {
+                type: "control",
+                modeSwitchButton: false,
+                editButton: false,
+                headerTemplate: function() {
+                    return $("<button>").attr("type", "button").text("Add")
+                            .on("click", function () {
+                                showDetailsDialog("Add", {});
+                            });
+                }
+            }
         ]
     });
 
+    $("#detailsForm").dialog({
+        autoOpen: false,
+        width: 400
+    });
 
-    $("#add").click(function() {
-        var newItem = {
+    var showDetailsDialog = function(dialogType, client) {
+        $("#name").val(client.Name);
+        $("#age").val(client.Age);
+        $("#address").val(client.Address);
+        $("#country").val(client.Country);
+        $("#married").prop("checked", client.Married);
+
+        $("#save").off("click").on("click", function() {
+            saveClient(client, dialogType === "Add");
+        });
+
+        $("#detailsForm").dialog("option", "title", dialogType + " Client")
+                .dialog("open");
+    };
+
+    var saveClient = function(client, isNew) {
+        $.extend(client, {
             Name: $("#name").val(),
             Age: parseInt($("#age").val(), 10),
             Address: $("#address").val(),
             Country: parseInt($("#country").val(), 10),
             Married: $("#married").is(":checked")
-        };
+        });
 
-        $("#jsGrid").jsGrid("insertItem", newItem);
+        $("#jsGrid").jsGrid(isNew ? "insertItem" : "updateItem", client);
 
-        $("#name").val("");
-        $("#age").val("");
-        $("#address").val("");
-        $("#country").val("0");
-        $("#married").prop("checked", false);
-    });
+        $("#detailsForm").dialog("close");
+    };
 
 });
