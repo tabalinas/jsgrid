e0c8b544ec7c9228bde6755bca9773e8663f9cdb
René Hézser <rene@hezser.de>
2015-03-13 15:26:55 +0100

    sort strategy more robust
    check for undefined or null

--- src/jsgrid.sort-strategies.js
+++ src/jsgrid.sort-strategies.js
@@ -1,23 +1,32 @@
-(function(jsGrid, $, undefined) {
-    
-    var sortStrategies = {
-        string: function(str1, str2) {
-            return str1.localeCompare(str2);
-        },
+(function (jsGrid, $, undefined) {
 
-        number: function(n1, n2) {
-            return n1 - n2;
-        },
+	var sortStrategies = {
+		string: function (str1, str2) {
+			if ((typeof (str1) == 'undefined' || str1 == null) && (typeof (str2) == 'undefined' || str2 == null))
+				return 0;
 
-        date: function(dt1, dt2) {
-            return dt1 - dt2;
-        },
-        
-        numberAsString: function(n1, n2) {
-            return parseFloat(n1) - parseFloat(n2);
-        }
-    };
+			if (typeof (str1) == 'undefined' || str1 == null)
+				return -1;
 
-    jsGrid.sortStrategies = sortStrategies;
-    
+			if (typeof (str2) == 'undefined' || str2 == null)
+				return 1;
+
+			return str1.localeCompare(str2);
+		},
+
+		number: function (n1, n2) {
+			return n1 - n2;
+		},
+
+		date: function (dt1, dt2) {
+			return dt1 - dt2;
+		},
+
+		numberAsString: function (n1, n2) {
+			return parseFloat(n1) - parseFloat(n2);
+		}
+	};
+
+	jsGrid.sortStrategies = sortStrategies;
+
 }(jsGrid, jQuery));
